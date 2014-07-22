11544a968156243b8f8857d53e656faa6d422f60
Alexander lisachenko <lisachenko.it@gmail.com>
2014-07-22 10:47:54 +0300

    Change the front-controller to enable AOP

--- web/index.php
+++ web/index.php
@@ -1,11 +1,15 @@
 <?php
 
+use Go\Instrument\Transformer\FilterInjectorTransformer;
+
 // comment out the following two lines when deployed to production
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+require(__DIR__ . '/aspect.php');
+require(FilterInjectorTransformer::rewrite(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php'));
 
 $config = require(__DIR__ . '/../config/web.php');
 
